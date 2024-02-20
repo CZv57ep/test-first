d8111b5a494ab345cdedf5de4351319e17d72732
smk <smk@majck.com>
2024-02-20 12:01:04 -0500

    tooling: version generation, setting, and tagging
    [Did not take on last commit] Rename get_version_str.py to set_version.py, which now generates the minor version, sets it in poetry, and creates the git tag.

--- set_version.py
+++ set_version.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
+from subprocess import run
 
 tsn = datetime.now()
-print(f"{tsn.year}.{tsn.toordinal()}.0")
+sem_ver = f"{tsn.year}.{tsn.toordinal()}.0"
+
+run(["poetry", "version", sem_ver], check=True)  # noqa: S603, S607
+run(["git", "tag", sem_ver], check=True)  # noqa: S603, S607
