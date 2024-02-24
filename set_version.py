a0dfd55847405d33f260e85811ab4bf0ebf23483
smk <smk@majck.com>
2024-02-24 14:40:47 -0500

    feature: check git status before updating version

--- set_version.py
+++ set_version.py
@@ -6,5 +6,11 @@ from subprocess import run
 tsn = datetime.now()
 sem_ver = f"{tsn.year}.{tsn.toordinal()}.0"
 
-run(["poetry", "version", sem_ver], check=True)  # noqa: S603, S607
-run(["git", "tag", sem_ver], check=True)  # noqa: S603, S607
+rc = run("git status".split(), check=True)  # noqa: S603
+if not rc.returncode:
+    run(["poetry", "version", sem_ver], check=True)  # noqa: S603, S607
+    run(["git", "tag", sem_ver], check=True)  # noqa: S603, S607
+else:
+    raise RuntimeError(
+        "Repository has uncommitted changes. Commit changes before updating package version."
+        )
