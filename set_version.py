e9ac7599f8e90202f515683740a2bf828887a89b
smk <smk@majck.com>
2024-03-03 15:10:03 -0500

    feature(set_version): Implement version update selection
    Implement facility to update full version number or patch version
    number; "full" version number updates are minor version number updates
    within a given calendar year, major version number udpates across a
    calnedar year boundary; do not proceed with version update if
    repository has uncommited changes.
    
    BREAKING CHANGE: ./set_version.py now requires an argument, "full" or
    "patch". Will not proceed if repository has uncommited changes.

--- set_version.py
+++ set_version.py
@@ -1,17 +1,53 @@
 #!/usr/bin/env python3
 
+import argparse
 from datetime import datetime
+from pathlib import Path
 from subprocess import run
 
-tsn = datetime.now()
-sem_ver = f"{tsn.year}.{tsn.toordinal()}.0"
+import re2 as re
+from tomlkit import parse
 
-rc = run("git status".split(), check=True)  # noqa: S603
-if not rc.returncode:
-    run(["poetry", "version", sem_ver], check=True)  # noqa: S603, S607
-    run(["git", "tag", sem_ver], check=True)  # noqa: S603, S607
-    run(["git", "commit", "pyproject.toml", "-m", '"chore: bump version number"'], check=True)  # noqa: S603, S607
-else:
+# Set up the argument parser
+parser = argparse.ArgumentParser(
+    description="Updates package version number, and commits and tags repository. User must specify `full` or `patch` level update."
+)
+parser.add_argument(
+    "patch_level", type=str, help="Whether `full` or `patch` level version update."
+)
+
+args = parser.parse_args()
+
+tsn = datetime.now()
+
+
+rc = run("git status".split(), check=True, capture_output=True, text=True)  # noqa: S603
+if re.search("modified:", rc.stdout):
     raise RuntimeError(
         "Repository has uncommitted changes. Commit changes before updating package version."
+    )
+
+
+def _get_pkg_version(_toml_path: str = "pyproject.toml") -> str:
+    _toml_dict = parse(Path(_toml_path).read_text())
+    return _toml_dict["tool"]["poetry"]["version"]  # type: ignore
+
+
+if args.patch_level == "patch":
+    run(["poetry", "version", "patch"], check=True)  # noqa: S603, S607
+    sem_ver = _get_pkg_version()
+else:
+    sem_ver = f"{tsn.year}.{tsn.toordinal()}.0"
+
+    if sem_ver == _get_pkg_version():
+        raise ValueError(
+            f"Package already at version, {pkg_version}. Perhaps update patch-level."
         )
+
+    run(["poetry", "version", sem_ver], check=True)  # noqa: S603, S607
+
+run(["git", "tag", f"{sem_ver}"], check=True)  # noqa: S603, S607
+run(
+    ["git", "commit", "pyproject.toml", "-m", '"chore: bump version number"'],  # noqa: S603, S607
+    check=True,
+)
