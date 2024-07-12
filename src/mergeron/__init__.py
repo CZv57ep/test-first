844ee34d1354144fc0db3d37f2ac74724a50c399
smk <smk@majck.com>
2024-07-12 08:51:12 -0400

    refactor: versioning, debugging messages
    Hardcode the version number so that the package need no longer be
    installed in the development venv, only test venvs; configure
    icecream to pring printing debugging messages; add geometric means
    as aggregators

--- src/mergeron/__init__.py
+++ src/mergeron/__init__.py
@@ -1,12 +1,19 @@
 from __future__ import annotations
 
 import enum
-from importlib.metadata import version
 from pathlib import Path
+from typing import Any
+
+import numpy as np
+import pendulum  # type: ignore
+from icecream import argumentToString, ic, install  # type: ignore
+from numpy.typing import NDArray
 
 _PKG_NAME: str = Path(__file__).parent.stem
 
-__version__ = version(_PKG_NAME)
+VERSION = "2024.738973.3"
+
+__version__ = VERSION
 
 DATA_DIR: Path = Path.home() / _PKG_NAME
 """
@@ -14,9 +21,24 @@ Defines a subdirectory named for this package in the user's home path.
 
 If the subdirectory doesn't exist, it is created on package invocation.
 """
-
 if not DATA_DIR.is_dir():
     DATA_DIR.mkdir(parents=False)
+
+
+np.set_printoptions(precision=18)
+
+
+def _timestamper() -> str:
+    return f"{pendulum.now().strftime("%F %T.%f")} |>  "
+
+
+@argumentToString.register(np.ndarray)  # type: ignore
+def _(_obj: NDArray[Any]) -> str:
+    return f"ndarray, shape={_obj.shape}, dtype={_obj.dtype}"
+
+
+ic.configureOutput(prefix=_timestamper, includeContext=True)
+install()
 
 
 @enum.unique
@@ -38,8 +60,11 @@ class UPPAggrSelector(enum.StrEnum):
     AVG = "average"
     CPA = "cross-product-share weighted average"
     CPD = "cross-product-share weighted distance"
+    CPG = "cross-product-share weighted geometric mean"
     DIS = "symmetrically-weighted distance"
+    GMN = "geometric mean"
     MAX = "max"
     MIN = "min"
     OSA = "own-share weighted average"
     OSD = "own-share weighted distance"
+    OSG = "own-share weighted geometric mean"
