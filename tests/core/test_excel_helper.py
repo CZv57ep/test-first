754bf3ab25108f3f22c981572c0afac6da898b22
smk <smk@majck.com>
2024-07-19 13:19:54 -0400

    refactor(test.core.test_excel_helper): add test of ragged array write

--- tests/core/test_excel_helper.py
+++ tests/core/test_excel_helper.py
@@ -1,16 +1,19 @@
 import gc
+from collections.abc import Sequence
 from pathlib import Path
+from typing import Any
 
 import mergeron.core.excel_helper as xlh
 import numpy as np
 import pytest
+from numpy.typing import NDArray
 from openpyxl import load_workbook
 from pytest import TempPathFactory
 from xlsxwriter import Workbook  # type: ignore
 
 
 @pytest.fixture(scope="session")
-def excel_file_path(tmp_path_factory: TempPathFactory) -> Path:
+def _excel_file_path(tmp_path_factory: TempPathFactory) -> Path:
     """Create a path to the test workbook, for writing to and reading from it
 
     See, https://docs.pytest.org/en/7.1.x/how-to/tmp_path.html
@@ -19,7 +22,15 @@ def excel_file_path(tmp_path_factory: TempPathFactory) -> Path:
     return _fpath
 
 
-def test_excel(excel_file_path: Path) -> None:
+@pytest.mark.parametrize(
+    "_data_val",
+    (
+        np.arange(16).reshape(8, 2),
+        [(1, 2), (3, [4, 5, 6]), (5, 6), (7, 8), (9, 10), (11, 12), (13, 14), (15, 16)],
+    ),
+)
+
+def test_excel(_data_val: NDArray[np.int64] | Sequence[Sequence[Any]], _excel_file_path: Path) -> None:
     """
     Test functions to create Excel instance and write to worksheet.
     """
@@ -27,7 +38,7 @@ def test_excel(excel_file_path: Path) -> None:
     _hdr_val = ("col1", "col2")
     _data_val = np.arange(16).reshape(8, 2)
 
-    _xl_book = Workbook(excel_file_path)
+    _xl_book = Workbook(_excel_file_path)
     _xl_sheet = _xl_book.add_worksheet()
 
     for _col_idx in range(2):
@@ -53,16 +64,16 @@ def test_excel(excel_file_path: Path) -> None:
     _xl_book.close()
     del _xl_book
 
-    _xl_book = load_workbook(excel_file_path)
+    _xl_book = load_workbook(_excel_file_path)
     _xl_sheet = _xl_book[_xl_book.sheetnames[0]]
     _xl_sheet_vals = tuple(_xl_sheet.values)
     _xl_book.close()
 
     try:
         assert all((  # noqa: S101
-            _xl_sheet_vals[0] == _hdr_val,
-            np.array_equal(np.array(_xl_sheet_vals[1:]), _data_val),
-        ))
+                    _xl_sheet_vals[0] == _hdr_val,
+                    np.array_equal(np.array(_xl_sheet_vals[1:]), _data_val),
+                    ))
     except AssertionError as _aer:
         print(_xl_sheet_vals, _hdr_val, _data_val)
         raise _aer
